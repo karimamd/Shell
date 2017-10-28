1ff215c50b7a5585692445c299e80fdf7a2acc14
Kareem Ahmed Abdel-salam <karimamd95@yahoo.com>
2017-10-28 08:49:16 +0200

    fixed some stuff

--- main.c
+++ main.c
@@ -55,26 +55,52 @@ char** str_split(char* inputLine, const char delimiter)
 int main() {
     char * inputLine;
     char **commandParts;
+    int numberOfArguments=0;
     //shell runs while exit command not written
     //TODO replace inputLine with command (after you parse)
-    while(strcmp(inputLine,"exit")!=0)
+    while(strcmp(inputLine,"exit")!=0 )
     {
         printf("Shell> ");
+        //TODO manage case of entering more than 512 chars
         char checkSuccess=fgets(inputLine,512,stdin);
+        //removing extra line \n
+        inputLine[strlen(inputLine)-1]='\0';
+        //splitting line into command and arguments
         commandParts=str_split(inputLine,' ');
 
-        //printing it
-        if (commandParts)
+
+        //put all code in here: it will execute if first word isn't exit
+        if(strcmp(commandParts[0],"exit")!=0 )
         {
+
             int i;
+            //free all dynamically allocated variables and print content
             for (i = 0; *(commandParts + i); i++)
             {
-                printf("command parts=[%s] \n", *(commandParts + i));
+                //TODO resolve the extra line problem on entering command
+                printf("\ncommand parts=[%s]", *(commandParts + i));
                 free(*(commandParts + i));
             }
             printf("\n");
             free(commandParts);
+
         }
+        else{
+            printf("exiting shell ....");
+            //inputLine[4]='\0';
+            int i;
+            //free all dynamically allocated variables
+            for (i = 0; *(commandParts + i); i++)
+            {
+                free(*(commandParts + i));
+            }
+            printf("\n");
+            free(commandParts);
+
+        }
+
+
+
 
     }
     return 0;
